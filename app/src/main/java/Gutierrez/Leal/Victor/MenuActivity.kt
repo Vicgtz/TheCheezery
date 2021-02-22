77cd4edbe07266923ae635f7ba20da72adf5da4e
Vicgtz09 <65575428+Vicgtz09@users.noreply.github.com>
2021-02-21 17:27:08 -0700

    Pantallas terminadas

---
+++ app/src/main/java/Gutierrez/Leal/Victor/MenuActivity.kt
@@ -0,0 +1,11 @@
+package Gutierrez.Leal.Victor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
